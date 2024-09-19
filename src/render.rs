681ee50e6cc8c078f29ba2f3c298ca40768f30bb
William Woodruff <william@yossarian.net>
2024-09-19 21:25:29 +0200

    WIP colorization
    Signed-off-by: William Woodruff <william@yossarian.net>

--- src/render.rs
+++ src/render.rs
@@ -1,7 +1,35 @@
 use std::io;
 
-use crate::finding::Finding;
+use crate::finding::{Confidence, Finding, Severity};
 use anyhow::Result;
+use colored::{ColoredString, Colorize};
+
+trait Colorized {
+    fn render(&self) -> ColoredString;
+}
+
+impl Colorized for Confidence {
+    fn render(&self) -> ColoredString {
+        match self {
+            Confidence::Unknown => "unknown".magenta(),
+            Confidence::Low => "low".yellow(),
+            Confidence::Medium => "medium".cyan(),
+            Confidence::High => "high".red(),
+        }
+    }
+}
+
+impl Colorized for Severity {
+    fn render(&self) -> ColoredString {
+        match self {
+            Severity::Unknown => "unknown".magenta(),
+            Severity::Informational => "informational".green(),
+            Severity::Low => "low".green(),
+            Severity::Medium => "medium".cyan(),
+            Severity::High => "high".red(),
+        }
+    }
+}
 
 pub(crate) fn render_findings<W>(writer: W, findings: &[Finding]) -> Result<()>
 where
@@ -23,11 +51,21 @@ where
     let mut writer = writer;
     writeln!(
         &mut writer,
-        "{ident} ({confidence:?} confidence, {severity:?} severity)",
+        "{ident} (C: {confidence}, S: {severity})",
         ident = finding.ident,
-        confidence = finding.determinations.confidence,
-        severity = finding.determinations.severity,
+        confidence = finding.determinations.confidence.render(),
+        severity = finding.determinations.severity.render(),
     )?;
+
+    for location in &finding.locations {
+        writeln!(
+            &mut writer,
+            "in {workflow}:{line}:{col}",
+            workflow = location.symbolic.name,
+            line = location.concrete.location.start_point.row,
+            col = location.concrete.location.start_point.column,
+        )?;
+    }
 
     Ok(())
 }
