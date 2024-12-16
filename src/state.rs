6a6e0ca614342ee17894785993c4bfc848b4d1fe
William Woodruff <william@yossarian.net>
2024-12-15 20:52:22 -0500

    refactor: use http-cache for caching, optimize network calls (#304)
    * refactor: use http-cache for caching
    
    * get it working
    
    * use tokio MT
    
    Not sure this matters, given that it's all essentially
    serialized to the same thread anyways.
    
    * optimize impostor-commit lookup pattern
    
    * ref-confusion: optimize GH API use
    
    * use an appropriate cache dir
    
    * docs: document cache
    
    * fix test
    
    * perf: reuse the same branches API

--- src/state.rs
+++ src/state.rs
@@ -1,24 +1,37 @@
 //! zizmor's runtime state, including application-level caching.
 
-use moka::sync::Cache;
+use std::path::PathBuf;
 
-use crate::{
-    github_api::{Branch, Client, ComparisonStatus, Tag},
-    App,
-};
+use etcetera::{choose_app_strategy, AppStrategy, AppStrategyArgs};
+
+use crate::{github_api::Client, App};
 
 #[derive(Clone)]
 pub(crate) struct AuditState {
     pub(crate) no_online_audits: bool,
+    pub(crate) cache_dir: PathBuf,
     pub(crate) gh_token: Option<String>,
-    pub(crate) caches: Caches,
 }
 
 impl AuditState {
     pub(crate) fn new(app: &App) -> Self {
+        let cache_dir = match &app.cache_dir {
+            Some(cache_dir) => cache_dir.as_std_path().to_path_buf(),
+            None => choose_app_strategy(AppStrategyArgs {
+                top_level_domain: "io.github".into(),
+                author: "woodruffw".into(),
+                app_name: "zizmor".into(),
+            })
+            // NOTE: no point in failing gracefully here.
+            .expect("failed to determine default cache directory")
+            .cache_dir(),
+        };
+
+        tracing::debug!("using cache directory: {cache_dir:?}");
+
         Self {
-            caches: Caches::new(),
             no_online_audits: app.no_online_audits,
+            cache_dir,
             gh_token: app.gh_token.clone(),
         }
     }
@@ -28,36 +41,6 @@ impl AuditState {
     pub(crate) fn github_client(&self) -> Option<Client> {
         self.gh_token
             .as_ref()
-            .map(|token| Client::new(token, self.caches.clone()))
-    }
-}
-
-/// Runtime caches.
-#[derive(Clone)]
-pub(crate) struct Caches {
-    /// A cache of `(owner, repo) => branches`.
-    pub(crate) branch_cache: Cache<(String, String), Vec<Branch>>,
-
-    /// A cache of `(owner, repo) => tags`.
-    pub(crate) tag_cache: Cache<(String, String), Vec<Tag>>,
-
-    /// A cache of `(base_ref, head_ref) => status`.
-    ///
-    /// We don't bother disambiguating this cache by `owner/repo`, since
-    /// `head_ref` is a SHA ref and we expect those to be globally unique.
-    /// This is not technically true of Git SHAs due to SHAttered, but is
-    /// effectively true for SHAs on GitHub due to GitHub's collision detection.
-    pub(crate) ref_comparison_cache: Cache<(String, String), Option<ComparisonStatus>>,
-}
-
-impl Caches {
-    pub(crate) fn new() -> Self {
-        Self {
-            // TODO: Increase these empirically? Would be good to have
-            // stats on how many unique repo slugs an average run sees.
-            branch_cache: Cache::new(1000),
-            tag_cache: Cache::new(1000),
-            ref_comparison_cache: Cache::new(10000),
-        }
+            .map(|token| Client::new(token, &self.cache_dir))
     }
 }
