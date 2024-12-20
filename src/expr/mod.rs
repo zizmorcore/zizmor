d9f98a5845c9571dbc85fa8abe54d9e8d1025728
William Woodruff <william@yossarian.net>
2024-12-20 19:49:56 +0000

    Revert "fix: handle context accesses following calls"
    This reverts commit 4b8fc3f73926e6f3d4fa38625cfab60b1b74ac78.

--- src/expr/mod.rs
+++ src/expr/mod.rs
@@ -365,7 +365,6 @@ mod tests {
             "(true == false) == true",
             "(true == (false || true && (true || false))) == true",
             "(github.actor != 'github-actions[bot]' && github.actor) == 'BrewTestBot'",
-            "fromJson(steps.runs.outputs.data).workflow_runs[0].id",
         ];
 
         for case in cases {
@@ -414,19 +413,17 @@ mod tests {
             ("foo.bar.baz", Expr::Context("foo.bar.baz".into())),
             (
                 "foo.bar.baz[1][2]",
-                Expr::Context("foo.bar.baz[1][2]".into()),
-                // Expr::Index {
-                //     parent: Expr::Context("foo.bar.baz".into()).into(),
-                //     indices: vec![Expr::Number(1.0), Expr::Number(2.0)],
-                // },
+                Expr::Index {
+                    parent: Expr::Context("foo.bar.baz".into()).into(),
+                    indices: vec![Expr::Number(1.0), Expr::Number(2.0)],
+                },
             ),
             (
                 "foo.bar.baz[*]",
-                Expr::Context("foo.bar.baz[*]".into()),
-                // Expr::Index {
-                //     parent: Expr::Context("foo.bar.baz".into()).into(),
-                //     indices: vec![Expr::Star],
-                // },
+                Expr::Index {
+                    parent: Expr::Context("foo.bar.baz".into()).into(),
+                    indices: vec![Expr::Star],
+                },
             ),
             (
                 "vegetables.*.ediblePortions",
@@ -476,18 +473,7 @@ mod tests {
                     op: BinOp::Or, rhs: Expr::Boolean(false).into()
                     }.into()
                 }
-            ),
-            (
-                // BUG: AST is incorrect here
-                "fromJson(steps.runs.outputs.data).workflow_runs[0].id",
-                Expr::Call {
-                    func: "fromJson".into(),
-                    args: vec![
-                        Expr::Context("steps.runs.outputs.data".into()).into(),
-                        Expr::Context("workflow_runs[0].id".into()).into(),
-                    ]
-                }
-            ),
+            )
         ];
 
         for (case, expr) in cases {
