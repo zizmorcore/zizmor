2f57215c9c1dbb90029b2c6cfb2a13bf19c70fcf
Jamison Lahman <jamison@lahman.dev>
2024-12-21 01:43:25 -0800

    fix: setup-go's cache-control is OptOut

--- src/audit/cache_poisoning.rs
+++ src/audit/cache_poisoning.rs
@@ -54,7 +54,7 @@ static KNOWN_CACHE_AWARE_ACTIONS: LazyLock<Vec<CacheAwareAction>> = LazyLock::ne
         // https://github.com/actions/setup-go/blob/main/action.yml
         CacheAwareAction {
             uses: Uses::from_step("actions/setup-go").unwrap(),
-            cache_control: CacheControl::OptIn("cache"),
+            cache_control: CacheControl::OptOut("cache"),
             control_value: ControlValue::String,
             caching_by_default: true,
         },
