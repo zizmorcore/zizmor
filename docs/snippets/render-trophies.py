eaa70a0ed5761761718e06835d25de7513df22a6
William Woodruff <william@yossarian.net>
2025-01-05 17:26:08 -0500

    docs: compress trophies

--- docs/snippets/render-trophies.py
+++ docs/snippets/render-trophies.py
@@ -3,18 +3,23 @@
 # render-trophies: take trophies.txt and produce a pretty
 # mkdocs-material card grid list from it
 
+from collections import defaultdict
 from pathlib import Path
 
 _TROPHIES = Path(__file__).parent / "trophies.txt"
 
 _TEMPLATE = """
--   ![](https://github.com/{org}.png?size=40){{ width=\"40\" loading=lazy align=left }} {org}/{repo}
+-   ![](https://github.com/{org}.png?size=40){{ width=\"40\" loading=lazy align=left }} {org}
 
     ---
 
-    {trophy}"""
+    ??? example "Examples"
+{trophies}
+"""
 
-for trophy in sorted(_TROPHIES.open().readlines()):
+by_org = defaultdict(list)
+
+for trophy in _TROPHIES.open().readlines():
     trophy = trophy.strip()
     if not trophy or trophy.startswith("#"):
         continue
@@ -25,6 +30,11 @@ for trophy in sorted(_TROPHIES.open().readlines()):
     else:
         repo, _ = rest.split("@")
 
+    by_org[org].append(trophy)
+
+
+for org, trophies in sorted(by_org.items(), key=lambda t: t[0].lower()):
     # NOTE: We request 40x40 from GitHub, but sometimes it gives us a bigger one.
     # Consequently, we also style with `width` to keep things consistent.
-    print(_TEMPLATE.format(org=org, repo=repo, trophy=trophy))
+    trophies = [f"        - {trophy}" for trophy in trophies]
+    print(_TEMPLATE.format(org=org, trophies="\n".join(trophies)))
