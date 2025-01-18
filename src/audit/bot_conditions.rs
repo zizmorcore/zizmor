ba568529d3d8c92beff7096125dd76788a86c1e6
William Woodruff <william@yossarian.net>
2025-01-17 22:18:01 -0500

    make it work

--- src/audit/bot_conditions.rs
+++ src/audit/bot_conditions.rs
@@ -2,7 +2,7 @@ use github_actions_models::common::{expr::ExplicitExpr, If};
 
 use crate::{
     expr::{self, Expr},
-    finding::{Confidence, Persona},
+    finding::{Confidence, Severity},
     models::JobExt,
 };
 
@@ -24,6 +24,8 @@ impl Audit for BotConditions {
         &self,
         job: &super::NormalJob<'w>,
     ) -> anyhow::Result<Vec<super::Finding<'w>>> {
+        let mut findings = vec![];
+
         // TODO: Consider other triggers as well?
         // In practice we expect to mostly see this problem with `pull_request_target`
         // triggers inside of "automerge this Dependabot PR"-style workflows.
@@ -31,49 +33,106 @@ impl Audit for BotConditions {
             return Ok(vec![]);
         }
 
+        let mut conds = vec![];
         if let Some(If::Expr(expr)) = &job.r#if {
-            if let Some((confidence, persona)) = Self::bot_condition(expr) {
-                todo!()
-            }
-        };
+            conds.push((expr, job.location()));
+        }
 
-        todo!()
+        for step in job.steps() {
+            if let Some(If::Expr(expr)) = &step.r#if {
+                conds.push((expr, step.location()));
+            }
+        }
+
+        for (expr, loc) in conds {
+            if let Some(confidence) = Self::bot_condition(expr) {
+                findings.push(
+                    Self::finding()
+                        .severity(Severity::High)
+                        .confidence(confidence)
+                        .add_location(
+                            loc.with_keys(&["if".into()])
+                                .primary()
+                                .annotated("github.actor may be spoofable"),
+                        )
+                        .build(job.parent())?,
+                );
+            }
+        }
+
+        Ok(findings)
     }
 }
 
 impl BotConditions {
-    fn walk_tree(expr: &Expr, dominating: bool) -> (bool, bool) {
+    fn walk_tree_for_bot_condition(expr: &Expr, dominating: bool) -> (bool, bool) {
         match expr {
             // We can't easily analyze the call's semantics, but we can
             // check to see if any of the call's arguments are
             // bot conditions. We treat a call as non-dominating always.
-            Expr::Call { func: _, args } => args
+            Expr::Call {
+                func: _,
+                args: exprs,
+            }
+            | Expr::Context {
+                raw: _,
+                components: exprs,
+            } => exprs
                 .iter()
-                .map(|arg| Self::walk_tree(arg, false))
+                .map(|arg| Self::walk_tree_for_bot_condition(arg, false))
                 .reduce(|(bc, _), (bc_next, _)| (bc || bc_next, false))
                 .unwrap_or((false, dominating)),
-            Expr::Index(expr) => Self::walk_tree(expr, dominating),
-            Expr::Context { raw: _, components } => todo!(),
+            Expr::Index(expr) => Self::walk_tree_for_bot_condition(expr, dominating),
             Expr::BinOp { lhs, op, rhs } => match op {
-                // && is non-dominating.
-                expr::BinOp::And => todo!(),
                 // || is dominating.
-                expr::BinOp::Or => todo!(),
+                expr::BinOp::Or => {
+                    let (bc_lhs, _) = Self::walk_tree_for_bot_condition(lhs, true);
+                    let (bc_rhs, _) = Self::walk_tree_for_bot_condition(rhs, true);
+
+                    (bc_lhs || bc_rhs, true)
+                }
                 // == is trivially dominating.
-                expr::BinOp::Eq => todo!(),
-                // Unclear.
-                expr::BinOp::Neq => todo!(),
-                expr::BinOp::Gt => todo!(),
-                expr::BinOp::Ge => todo!(),
-                expr::BinOp::Lt => todo!(),
-                expr::BinOp::Le => todo!(),
+                expr::BinOp::Eq => match (lhs.as_ref(), rhs.as_ref()) {
+                    (Expr::Context { raw, .. }, Expr::String(s))
+                    | (Expr::String(s), Expr::Context { raw, .. }) => {
+                        if raw == "github.actor" && s.ends_with("[bot]") {
+                            (true, true)
+                        } else {
+                            (false, true)
+                        }
+                    }
+                    (lhs, rhs) => {
+                        let (bc_lhs, _) = Self::walk_tree_for_bot_condition(lhs, true);
+                        let (bc_rhs, _) = Self::walk_tree_for_bot_condition(rhs, true);
+
+                        (bc_lhs || bc_rhs, true)
+                    }
+                },
+                // Every other binop is non-dominating.
+                _ => {
+                    let (bc_lhs, _) = Self::walk_tree_for_bot_condition(lhs, false);
+                    let (bc_rhs, _) = Self::walk_tree_for_bot_condition(rhs, false);
+
+                    (bc_lhs || bc_rhs, false)
+                }
             },
-            Expr::UnOp { op, expr } => todo!(),
+            Expr::UnOp { op, expr } => match op {
+                // We don't really know what we're negating, so naively
+                // assume we're non-dominating.
+                //
+                // TODO: This is slightly incorrect, since we should
+                // treat `!(github.actor == 'dependabot[bot]')` as a
+                // negative case even though it has an interior bot condition.
+                // However, to model this correctly we need to go from a
+                // boolean condition to a three-state: `Some(bool)` for
+                // an explicitly toggled condition, and `None` for no condition.
+                expr::UnOp::Not => Self::walk_tree_for_bot_condition(expr, false),
+            },
             _ => (false, dominating),
         }
     }
 
-    fn bot_condition(expr: &str) -> Option<(Confidence, Persona)> {
+    fn bot_condition(expr: &str) -> Option<Confidence> {
         let Ok(expr) = (match ExplicitExpr::from_curly(expr) {
             Some(raw_expr) => Expr::parse(raw_expr.as_bare()),
             None => Expr::parse(expr),
@@ -87,8 +146,55 @@ impl BotConditions {
         // expression truth value. For example, `github.actor == 'dependabot[bot]' || foo`
         // has the bot condition as dominating, since regardless of `foo` the check
         // always passes if the actor is dependabot[bot].
-        let (has_bot_condition, condition_dominates) = Self::walk_tree(&expr, true);
+        match Self::walk_tree_for_bot_condition(&expr, true) {
+            // We have a bot condition and it dominates the expression.
+            (true, true) => Some(Confidence::High),
+            // We have a bot condition but it doesn't dominate the expression.
+            (true, false) => Some(Confidence::Medium),
+            // No bot condition.
+            (_, _) => None,
+        }
+    }
+}
 
-        None
+#[cfg(test)]
+mod tests {
+    use crate::{audit::bot_conditions::BotConditions, finding::Confidence};
+
+    #[test]
+    fn test_bot_condition() {
+        for (cond, confidence) in &[
+            // Trivial dominating cases.
+            ("github.actor == 'dependabot[bot]'", Confidence::High),
+            ("'dependabot[bot]' == github.actor", Confidence::High),
+            // Dominating cases with OR.
+            (
+                "'dependabot[bot]' == github.actor || true",
+                Confidence::High,
+            ),
+            (
+                "'dependabot[bot]' == github.actor || false",
+                Confidence::High,
+            ),
+            (
+                "'dependabot[bot]' == github.actor || github.actor == 'foobar'",
+                Confidence::High,
+            ),
+            (
+                "github.actor == 'foobar' || 'dependabot[bot]' == github.actor",
+                Confidence::High,
+            ),
+            // Non-dominating cases with AND.
+            (
+                "'dependabot[bot]' == github.actor && something.else",
+                Confidence::Medium,
+            ),
+            (
+                "something.else && 'dependabot[bot]' == github.actor",
+                Confidence::Medium,
+            ),
+        ] {
+            assert_eq!(BotConditions::bot_condition(cond).unwrap(), *confidence);
+        }
     }
 }
