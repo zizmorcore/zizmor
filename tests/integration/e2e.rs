3fdfadbe78576ec132472d9392f080ca108d1e1b
William Woodruff <william@yossarian.net>
2025-02-26 23:41:31 -0500

    test: refactor integration tests
    This builds a top level `integration` crate,
    rather than separate binaries for each test.
    This both makes builds and tests faster
    (fewer compilation steps, fewer test execs)
    and eliminates spurious dead code warnings.
    
    See https://github.com/rust-lang/rust/issues/46379
    for inspiration.

---
+++ tests/integration/e2e.rs
@@ -0,0 +1,9 @@
+//! End-to-end snapshot integration tests.
+
+use anyhow::Result;
+
+#[cfg_attr(not(feature = "gh-token-tests"), ignore)]
+#[test]
+fn pyca_cryptography() -> Result<()> {
+    Ok(())
+}
