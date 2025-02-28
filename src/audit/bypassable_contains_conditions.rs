c780bebb4451e9ddbc6f169a41682ad3e3b7493e
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2025-03-01 00:37:56 +0100

    chore(audit): Improve warning messages and confidence

--- src/audit/bypassable_contains_conditions.rs
+++ src/audit/bypassable_contains_conditions.rs
@@ -2,10 +2,28 @@ use github_actions_models::common::{expr::ExplicitExpr, If};
 
 use super::{audit_meta, Audit};
 use crate::{
-    expr::{self, Context, Expr},
+    expr::{Context, Expr},
     finding::{Confidence, Severity},
     models::JobExt,
 };
+
+const USER_CONTROLLABLE_CONTEXTS: [&str; 15] = [
+    "env.GITHUB_ACTOR",
+    "env.GITHUB_BASE_REF",
+    "env.GITHUB_HEAD_REF",
+    "env.GITHUB_REF",
+    "env.GITHUB_REF_NAME",
+    "env.GITHUB_SHA",
+    "env.GITHUB_TRIGGERING_ACTOR",
+    "github.actor",
+    "github.base_ref",
+    "github.head_ref",
+    "github.ref",
+    "github.ref_name",
+    "github.sha",
+    "github.triggering_actor",
+    "inputs.",
+];
 
 pub(crate) struct BypassableContainsConditions;
 
@@ -44,15 +62,15 @@ impl Audit for BypassableContainsConditions {
             });
 
         conditions
-            .filter_map(|(expr, loc)| {
-                Self::insecure_contains(expr).map(|confidence| {
+            .flat_map(|(expr, loc)| {
+                Self::insecure_contains(expr).into_iter().map(move |(confidence, context)| {
                     Self::finding()
                         .severity(Severity::High)
                         .confidence(confidence)
                         .add_location(
                             loc.with_keys(&["if".into()])
                                 .primary()
-                                .annotated("contains condition can be bypassed"),
+                                .annotated(format!("contains(..) condition can be bypassed if attacker can control '{context}'")),
                         )
                         .build(job.parent())
                 })
@@ -62,16 +80,16 @@ impl Audit for BypassableContainsConditions {
 }
 
 impl BypassableContainsConditions {
-    fn walk_tree_for_insecure_contains(expr: &Expr) -> bool {
+    fn walk_tree_for_insecure_contains<'a>(
+        expr: &'a Expr,
+    ) -> Box<dyn Iterator<Item = (&'a str, &'a Context<'a>)> + 'a> {
         match expr {
             Expr::Call {
                 func: "contains",
                 args: exprs,
             } => match exprs.as_slice() {
-                [Expr::String(_), Expr::Context(_)] => true,
-                args => args
-                    .iter()
-                    .any(|arg| Self::walk_tree_for_insecure_contains(arg)),
+                [Expr::String(s), Expr::Context(c)] => Box::new(std::iter::once((s.as_str(), c))),
+                args => Box::new(args.iter().flat_map(Self::walk_tree_for_insecure_contains)),
             },
             Expr::Call {
                 func: _,
@@ -80,46 +98,41 @@ impl BypassableContainsConditions {
             | Expr::Context(Context {
                 raw: _,
                 components: exprs,
-            }) => exprs
-                .iter()
-                .map(|arg| Self::walk_tree_for_insecure_contains(arg))
-                .reduce(|bc, bc_next| (bc || bc_next))
-                .unwrap_or(false),
+            }) => Box::new(exprs.iter().flat_map(Self::walk_tree_for_insecure_contains)),
             Expr::Index(expr) => Self::walk_tree_for_insecure_contains(expr),
-            Expr::BinOp { lhs, op, rhs } => match op {
-                expr::BinOp::Or => {
-                    let bc_lhs = Self::walk_tree_for_insecure_contains(lhs);
-                    let bc_rhs = Self::walk_tree_for_insecure_contains(rhs);
+            Expr::BinOp { lhs, rhs, .. } => {
+                let bc_lhs = Self::walk_tree_for_insecure_contains(lhs);
+                let bc_rhs = Self::walk_tree_for_insecure_contains(rhs);
 
-                    bc_lhs || bc_rhs
-                }
-                _ => {
-                    let bc_lhs = Self::walk_tree_for_insecure_contains(lhs);
-                    let bc_rhs = Self::walk_tree_for_insecure_contains(rhs);
-
-                    bc_lhs || bc_rhs
-                }
-            },
-            Expr::UnOp { op, expr } => match op {
-                expr::UnOp::Not => Self::walk_tree_for_insecure_contains(expr),
-            },
-            _ => false,
+                Box::new(bc_lhs.chain(bc_rhs))
+            }
+            Expr::UnOp { expr, .. } => Self::walk_tree_for_insecure_contains(expr),
+            _ => Box::new(std::iter::empty()),
         }
     }
 
-    fn insecure_contains(expr: &str) -> Option<Confidence> {
+    fn insecure_contains(expr: &str) -> Vec<(Confidence, String)> {
         let bare = match ExplicitExpr::from_curly(expr) {
             Some(raw_expr) => raw_expr.as_bare().to_string(),
             None => expr.to_string(),
         };
 
-        let Ok(expr) = Expr::parse(&bare) else {
-            tracing::warn!("couldn't parse expression: {expr}");
-            return None;
-        };
-
-        // We're looking for `contains("something", context)` anywhere in the expression tree.
-        Self::walk_tree_for_insecure_contains(&expr).then_some(Confidence::High)
+        Expr::parse(&bare)
+            .inspect_err(|_err| tracing::warn!("couldn't parse expression: {expr}"))
+            .iter()
+            .flat_map(|expression| Self::walk_tree_for_insecure_contains(expression))
+            .map(|(_s, ctx)| {
+                let confidence = if USER_CONTROLLABLE_CONTEXTS.iter().any(|item| {
+                    let context = &ctx.as_str();
+                    item == context || (item.ends_with(".") && context.starts_with(item))
+                }) {
+                    Confidence::High
+                } else {
+                    Confidence::Medium
+                };
+                (confidence, ctx.as_str().to_string())
+            })
+            .collect()
     }
 }
 
@@ -132,15 +145,15 @@ mod tests {
     fn test_bot_condition() {
         for (cond, confidence) in &[
             // Vulnerable conditions
-            ("contains('refs/heads/main refs/heads/develop', github.ref)", Some(Confidence::High)),
-            ("false || contains('main,develop', github.head_ref)", Some(Confidence::High)),
-            ("!contains('main|develop', github.base_ref)", Some(Confidence::High)),
-            ("contains(fromJSON('[true]'), contains('refs/heads/main refs/heads/develop', env.GITHUB_REF))", Some(Confidence::High)),
+            ("contains('refs/heads/main refs/heads/develop', github.ref)", vec![(Confidence::High, String::from("github.ref"))]),
+            ("false || contains('main,develop', github.head_ref)", vec![(Confidence::High, String::from("github.head_ref"))]),
+            ("!contains('main|develop', github.base_ref)", vec![(Confidence::High, String::from("github.base_ref"))]),
+            ("contains(fromJSON('[true]'), contains('refs/heads/main refs/heads/develop', env.GITHUB_REF))", vec![(Confidence::High, String::from("env.GITHUB_REF"))]),
             // These are okay.
-            ("github.ref == 'refs/heads/main' || github.ref == 'refs/heads/develop'", None),
-            ("contains(fromJSON('[\"refs/heads/main\", \"refs/heads/develop\"]'), github.ref)", None),
+            ("github.ref == 'refs/heads/main' || github.ref == 'refs/heads/develop'", vec![]),
+            ("contains(fromJSON('[\"refs/heads/main\", \"refs/heads/develop\"]'), github.ref)", vec![]),
         ] {
-            assert_eq!(BypassableContainsConditions::insecure_contains(cond), *confidence);
+            assert_eq!(BypassableContainsConditions::insecure_contains(cond).as_slice(), confidence.as_slice());
         }
     }
 }
