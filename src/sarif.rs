02bb0beb5ff55ccb70186f966ba80761ba8ac0fc
William Woodruff <william@yossarian.net>
2025-03-23 01:56:08 +0000

    don't bother with file:// prefix
    Signed-off-by: William Woodruff <william@yossarian.net>

--- src/sarif.rs
+++ src/sarif.rs
@@ -70,11 +70,7 @@ fn build_run(findings: &[Finding], cwd: &Utf8Path) -> Run {
             // we include the `$CWD` as `invocations[0].working_directory.uri`,
             // which should make GitHub's viewer process relative paths
             // as if they were absolute.
-            .working_directory(
-                ArtifactLocation::builder()
-                    .uri(format!("file://{cwd}"))
-                    .build(),
-            )
+            .working_directory(ArtifactLocation::builder().uri(cwd).build())
             .build()])
         .build()
 }
