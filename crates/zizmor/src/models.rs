2e91ae671932d4a30ee42b7f9f9c8bdb43344fda
William Woodruff <william@yossarian.net>
2025-06-02 13:58:54 -0400

    clippy
    Signed-off-by: William Woodruff <william@yossarian.net>

--- crates/zizmor/src/models.rs
+++ crates/zizmor/src/models.rs
@@ -556,15 +556,10 @@ impl<'doc> Deref for Step<'doc> {
 
 impl<'doc> StepCommon<'doc> for Step<'doc> {
     fn env_is_static(&self, name: &str) -> bool {
-        // Collect each of the step, job, and workflow-level `env` blocks
-        // and check each.
-        let mut envs = vec![];
-
-        envs.push(&self.env);
-        envs.push(&self.job().env);
-        envs.push(&self.workflow().env);
-
-        utils::env_is_static(name, &envs)
+        utils::env_is_static(
+            name,
+            &[&self.env, &self.job().env, &self.workflow().env],
+        )
     }
 
     fn uses(&self) -> Option<&common::Uses> {
