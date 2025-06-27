ad7eeca14d09ea1d298a95b00df2244edda7243d
William Woodruff <william@yossarian.net>
2025-06-27 00:22:27 -0600

    hackety hack
    Signed-off-by: William Woodruff <william@yossarian.net>

--- crates/zizmor/src/lsp.rs
+++ crates/zizmor/src/lsp.rs
@@ -1,11 +1,19 @@
 //! zizmor's language server.
 
+use camino::Utf8Path;
 use tower_lsp::jsonrpc::Result;
-use tower_lsp::lsp_types::*;
+use tower_lsp::lsp_types;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
+use crate::audit::AuditInput;
 use crate::config::Config;
 use crate::{AuditRegistry, AuditState};
+
+struct LspDocumentCommon<'a> {
+    uri: lsp_types::Url,
+    text: &'a str,
+    version: Option<i32>,
+}
 
 #[derive(Debug)]
 struct Backend {
@@ -15,13 +23,61 @@ struct Backend {
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult::default())
+    async fn initialize(
+        &self,
+        _: lsp_types::InitializeParams,
+    ) -> Result<lsp_types::InitializeResult> {
+        Ok(lsp_types::InitializeResult {
+            server_info: Some(lsp_types::ServerInfo {
+                name: "zizmor (LSP)".into(),
+                version: Some(env!("CARGO_PKG_VERSION").into()),
+            }),
+            capabilities: lsp_types::ServerCapabilities {
+                text_document_sync: Some(lsp_types::TextDocumentSyncCapability::Options(
+                    lsp_types::TextDocumentSyncOptions {
+                        open_close: Some(true),
+                        change: Some(lsp_types::TextDocumentSyncKind::FULL),
+                        save: Some(lsp_types::TextDocumentSyncSaveOptions::SaveOptions(
+                            lsp_types::SaveOptions {
+                                include_text: Some(true),
+                            },
+                        )),
+                        ..Default::default()
+                    },
+                )),
+                ..Default::default()
+            },
+        })
     }
 
-    async fn initialized(&self, _: InitializedParams) {
+    async fn initialized(&self, _: lsp_types::InitializedParams) {
         self.client
-            .log_message(MessageType::INFO, "server initialized!")
+            .register_capability(vec![lsp_types::Registration {
+                id: "zizmor-cap-textdocumentregistrationoptions".into(),
+                method: "textDocument/didOpen".into(),
+                register_options: Some(
+                    serde_json::to_value(lsp_types::TextDocumentRegistrationOptions {
+                        document_selector: Some(vec![
+                            lsp_types::DocumentFilter {
+                                language: Some("yaml".into()),
+                                scheme: None,
+                                pattern: Some("**/.github/workflows/*.{yml,yaml}".into()),
+                            },
+                            lsp_types::DocumentFilter {
+                                language: Some("yaml".into()),
+                                scheme: None,
+                                pattern: Some("**/action.{yml,yaml}".into()),
+                            },
+                        ]),
+                    })
+                    .unwrap(),
+                ),
+            }])
+            .await
+            .expect("failed to register text document capabilities with the LSP client");
+
+        self.client
+            .log_message(lsp_types::MessageType::INFO, "server initialized!")
             .await;
     }
 
@@ -29,21 +85,55 @@ impl LanguageServer for Backend {
         Ok(())
     }
 
-    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+    async fn did_open(&self, params: lsp_types::DidOpenTextDocumentParams) {
         tracing::debug!("did_open: {:?}", params);
+        self.analyze(LspDocumentCommon {
+            uri: params.text_document.uri,
+            text: &params.text_document.text,
+            version: Some(params.text_document.version),
+        })
+        .await;
     }
 
-    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+    async fn did_change(&self, params: lsp_types::DidChangeTextDocumentParams) {
         tracing::debug!("did_change: {:?}", params);
+        let Some(change) = params.content_changes.first() else {
+            return;
+        };
+
+        self.analyze(LspDocumentCommon {
+            uri: params.text_document.uri,
+            text: &change.text,
+            version: Some(params.text_document.version),
+        })
+        .await;
     }
 
-    async fn did_save(&self, params: DidSaveTextDocumentParams) {
+    async fn did_save(&self, params: lsp_types::DidSaveTextDocumentParams) {
         tracing::debug!("did_save: {:?}", params);
+        if let Some(text) = &params.text {
+            self.analyze(LspDocumentCommon {
+                uri: params.text_document.uri,
+                text: &text,
+                version: None,
+            })
+            .await;
+        }
     }
 }
 
 impl Backend {
-    async fn analyze(&self, params: TextDocumentItem) {
+    async fn analyze<'a>(&self, params: LspDocumentCommon<'a>) {
+        tracing::debug!("analyzing: {:?} (version={:?})", params.uri, params.version);
+        let path = Utf8Path::new(params.uri.path());
+        let input = if matches!(path.file_name(), Some("action.yml" | "action.yaml")) {
+            todo!()
+        } else if matches!(path.extension(), Some("yml" | "yaml")) {
+            todo!()
+        } else {
+            return;
+        };
+
         // self.client.publish_diagnostics(uri, diags, version)
         todo!()
     }
