29a38a50d041a5c6c62471ae74f46c6735ff26f8
William Woodruff <william@yossarian.net>
2025-08-26 00:36:06 -0400

    more tests

--- crates/zizmor/tests/integration/config.rs
+++ crates/zizmor/tests/integration/config.rs
@@ -16,6 +16,23 @@ fn test_discovers_config_in_root() -> anyhow::Result<()> {
     Ok(())
 }
 
+/// Ensures we correctly discover a configuration file in the root
+/// directory from an input filename, i.e. going from
+/// `config-in-root/.github/workflows/hackme.yml`
+/// to `config-in-root/zizmor.yml` in this case.
+#[test]
+fn test_discovers_config_in_root_from_file_input() -> anyhow::Result<()> {
+    insta::assert_snapshot!(
+        zizmor()
+            .input(input_under_test(
+                "config-scenarios/config-in-root/.github/workflows/hackme.yml"
+            ))
+            .run()?
+    );
+
+    Ok(())
+}
+
 /// Ensures we ignore a configuration file in the root of a given
 /// input directory when `--no-config` is specified.
 #[test]
@@ -24,6 +41,22 @@ fn test_ignores_config_in_root() -> anyhow::Result<()> {
         zizmor()
             .no_config(true)
             .input(input_under_test("config-scenarios/config-in-root"))
+            .run()?
+    );
+
+    Ok(())
+}
+
+/// Ensures we ignore a configuration file in the root directory
+/// from an input filename when `--no-config` is specified.
+#[test]
+fn test_ignores_config_in_root_from_file_input() -> anyhow::Result<()> {
+    insta::assert_snapshot!(
+        zizmor()
+            .no_config(true)
+            .input(input_under_test(
+                "config-scenarios/config-in-root/.github/workflows/hackme.yml"
+            ))
             .run()?
     );
 
@@ -44,6 +77,23 @@ fn test_discovers_config_in_dotgithub() -> anyhow::Result<()> {
     Ok(())
 }
 
+/// Ensures we correctly discover a configuration file in a `.github`
+/// subdirectory from an input filename, i.e. going from
+/// `config-in-dotgithub/.github/workflows/hackme.yml`
+/// to `config-in-dotgithub/.github/zizmor.yml` in this case.
+#[test]
+fn test_discovers_config_in_dotgithub_from_file_input() -> anyhow::Result<()> {
+    insta::assert_snapshot!(
+        zizmor()
+            .input(input_under_test(
+                "config-scenarios/config-in-dotgithub/.github/workflows/hackme.yml"
+            ))
+            .run()?
+    );
+
+    Ok(())
+}
+
 /// Ensures we ignore a configuration file in a `.github` subdirectory
 /// of a given input directory when `--no-config` is specified.
 #[test]
@@ -52,6 +102,22 @@ fn test_ignores_config_in_dotgithub() -> anyhow::Result<()> {
         zizmor()
             .no_config(true)
             .input(input_under_test("config-scenarios/config-in-dotgithub"))
+            .run()?
+    );
+
+    Ok(())
+}
+
+/// Ensures we ignore a configuration file in a `.github` subdirectory
+/// from an input filename when `--no-config` is specified.
+#[test]
+fn test_ignores_config_in_dotgithub_from_file_input() -> anyhow::Result<()> {
+    insta::assert_snapshot!(
+        zizmor()
+            .no_config(true)
+            .input(input_under_test(
+                "config-scenarios/config-in-dotgithub/.github/workflows/hackme.yml"
+            ))
             .run()?
     );
 
