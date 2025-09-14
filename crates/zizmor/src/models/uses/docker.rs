99d1025690b5a5e2b086a9b31b5510d9c79d476c
William Woodruff <william@yossarian.net>
2025-09-14 00:29:17 -0400

    sketching
    Signed-off-by: William Woodruff <william@yossarian.net>

--- crates/zizmor/src/models/uses/docker.rs
+++ crates/zizmor/src/models/uses/docker.rs
@@ -1,1 +1,11 @@
-//! Patterns for Docker image `uses:` and corresponding extension traits.
+//! Patterns for Docker images (including in `uses:` clauses) and corresponding extension traits.
+
+pub(crate) enum DockerImagePattern {
+    /// Matches `registry/repo/*` or `repo/*` (in the default registry case),
+    /// i.e. any image in the given repository.
+    InRepo { registry: String, repo: String },
+    /// Matches `registry/*`, i.e. any image in the given registry.
+    InRegistry(String),
+    /// Matches any image.
+    Any,
+}
