8c77d42343a09408b7b6f04db0c9125137b465ff
Joe Wallwork <jw2423@cam.ac.uk>
2025-11-18 17:43:23 +0000

    Account for steps as well as the job

--- crates/zizmor/src/audit/timeout_minutes.rs
+++ crates/zizmor/src/audit/timeout_minutes.rs
@@ -15,7 +15,7 @@ pub(crate) struct TimeoutMinutes;
 audit_meta!(
     TimeoutMinutes,
     "timeout-minutes",
-    "missing timeout-minutes on jobs"
+    "missing timeout-minutes"
 );
 
 #[async_trait::async_trait]
@@ -31,24 +31,60 @@ impl Audit for TimeoutMinutes {
     ) -> anyhow::Result<Vec<Finding<'doc>>, AuditError> {
         let mut findings = vec![];
 
-        // Check if timeout-minutes is missing
-        match &job.timeout_minutes {
-            None => {
-                findings.push(
-                    Self::finding()
-                        .confidence(Confidence::High)
-                        .severity(Severity::Medium)
-                        .persona(Persona::Pedantic)
-                        .add_location(
-                            job
-                                .location()
-                                .primary()
-                                .annotated("missing timeout-minutes"),
-                        )
-                        .build(job.parent())?,
-                );
-            },
-            _ => {}
+        // Check if timeout-minutes is set in any of the job's steps
+        let mut found = false;
+        for step in job.steps() {
+            match &step.timeout_minutes {
+                Some(_) => {
+                    found = true;
+                    break;
+                }
+                None => {},
+            }
+        };
+
+        if found {
+            // If so, check if it's missing from any other steps
+            for step in job.steps() {
+                match &step.timeout_minutes {
+                    None => {
+                        findings.push(
+                            Self::finding()
+                                .confidence(Confidence::High)
+                                .severity(Severity::Medium)
+                                .persona(Persona::Pedantic)
+                                .add_location(
+                                    job
+                                        .location()
+                                        .primary()
+                                        .annotated("step missing timeout-minutes"),
+                                )
+                                .build(&step)?,
+                        );
+                    },
+                    _ => {}
+                }
+            }
+        } else {
+            // If not, check if timeout-minutes is missing from the job
+            match &job.timeout_minutes {
+                None => {
+                    findings.push(
+                        Self::finding()
+                            .confidence(Confidence::High)
+                            .severity(Severity::Medium)
+                            .persona(Persona::Pedantic)
+                            .add_location(
+                                job
+                                    .location()
+                                    .primary()
+                                    .annotated("job missing timeout-minutes"),
+                            )
+                            .build(job.parent())?,
+                    );
+                },
+                _ => {}
+            }
         }
 
         Ok(findings)
