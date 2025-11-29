dc026c038b8771eb6476ea5d0e1f6cdd16d24821
William Woodruff <william@yossarian.net>
2025-11-28 20:54:50 -0500

    move tests around
    Signed-off-by: William Woodruff <william@yossarian.net>

--- crates/zizmor/tests/integration/audit/forbidden_uses.rs
+++ crates/zizmor/tests/integration/audit/forbidden_uses.rs
@@ -1,4 +1,4 @@
-use crate::common::{input_under_test, zizmor};
+use crate::common::{OutputMode, input_under_test, zizmor};
 use anyhow::Result;
 
 #[test]
@@ -167,6 +167,64 @@ fn test_allow_some_refs() -> Result<()> {
        = note: audit confidence → High
 
     2 findings (1 suppressed): 0 informational, 0 low, 0 medium, 1 high
+    "
+    );
+
+    Ok(())
+}
+
+#[test]
+fn test_config_invalid_pattern() -> Result<()> {
+    insta::assert_snapshot!(
+        zizmor()
+            .expects_failure(true)
+            .input(input_under_test("neutral.yml"))
+            .config(input_under_test(
+                "forbidden-uses/configs/invalid-pattern.yml"
+            ))
+            .output(OutputMode::Stderr)
+            .run()?,
+        @r"
+    🌈 zizmor v@@VERSION@@
+    fatal: no audit was performed
+    error: configuration error in @@CONFIG@@
+      |
+      = help: check the configuration for the 'forbidden-uses' rule
+      = help: see: https://docs.zizmor.sh/audits/#forbidden-uses-configuration
+
+    Caused by:
+        0: configuration error in @@CONFIG@@
+        1: invalid syntax for audit `forbidden-uses`
+        2: invalid pattern: */*
+    "
+    );
+
+    Ok(())
+}
+
+#[test]
+fn test_config_invalid_variant() -> Result<()> {
+    insta::assert_snapshot!(
+        zizmor()
+            .expects_failure(true)
+            .input(input_under_test("neutral.yml"))
+            .config(input_under_test(
+                "forbidden-uses/configs/invalid-variant.yml"
+            ))
+            .output(OutputMode::Stderr)
+            .run()?,
+        @r"
+    🌈 zizmor v@@VERSION@@
+    fatal: no audit was performed
+    error: configuration error in @@CONFIG@@
+      |
+      = help: check the configuration for the 'forbidden-uses' rule
+      = help: see: https://docs.zizmor.sh/audits/#forbidden-uses-configuration
+
+    Caused by:
+        0: configuration error in @@CONFIG@@
+        1: invalid syntax for audit `forbidden-uses`
+        2: unknown variant `mystery-variant`, expected `allow` or `deny`
     "
     );
 
