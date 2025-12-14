147bfabc0fd4c9e3015f8b3443af5ad6eaf623ab
William Woodruff <william@yossarian.net>
2025-12-14 00:06:46 -0800

    ci: only run benchmarks on labeled PRs (#1442)

--- bench/test_bench_online.py
+++ bench/test_bench_online.py
@@ -6,63 +6,36 @@ from bench.common import zizmor
 
 
 @pytest.mark.skipif("GH_TOKEN" not in os.environ, reason="GH_TOKEN not set")
-def test_zizmor_online_grafana_9f212d11d0ac(benchmark):
-    """
-    Runs `zizmor --format=plain --no-exit-codes --no-config grafana/grafana@9f212d11d0ac`
-    """
-
-    benchmark.pedantic(
-        zizmor,
-        args=(
-            [
-                "--format=plain",
-                "--no-exit-codes",
-                "--no-config",
-                "grafana/grafana@9f212d11d0ac",
-            ],
-        ),
-        warmup_rounds=2,
-        iterations=10,
-    )
-
-
-@pytest.mark.skipif("GH_TOKEN" not in os.environ, reason="GH_TOKEN not set")
-def test_zizmor_online_gha_hazmat_da3c3cd(benchmark):
+@pytest.mark.benchmark
+def test_zizmor_online_gha_hazmat_da3c3cd():
     """
     Runs `zizmor --format=plain --no-exit-codes --no-config woodruffw/gha-hazmat@da3c3cd`
     """
 
-    benchmark.pedantic(
-        zizmor,
-        args=(
-            [
-                "--format=plain",
-                "--no-exit-codes",
-                "--no-config",
-                "woodruffw/gha-hazmat@da3c3cd",
-            ],
-        ),
-        warmup_rounds=2,
-        iterations=10,
+    zizmor(
+        [
+            "--format=plain",
+            "--no-exit-codes",
+            "--no-config",
+            "woodruffw/gha-hazmat@da3c3cd",
+        ],
+        check=True,
     )
 
 
 @pytest.mark.skipif("GH_TOKEN" not in os.environ, reason="GH_TOKEN not set")
-def test_zizmor_online_cpython_48f88310044c(benchmark):
+@pytest.mark.benchmark
+def test_zizmor_online_cpython_48f88310044c():
     """
     Runs `zizmor --format=plain --no-exit-codes --no-config python/cpython@48f88310044c`
     """
 
-    benchmark.pedantic(
-        zizmor,
-        args=(
-            [
-                "--format=plain",
-                "--no-exit-codes",
-                "--no-config",
-                "python/cpython@48f88310044c",
-            ],
-        ),
-        warmup_rounds=2,
-        iterations=10,
+    zizmor(
+        [
+            "--format=plain",
+            "--no-exit-codes",
+            "--no-config",
+            "python/cpython@48f88310044c",
+        ],
+        check=True,
     )
