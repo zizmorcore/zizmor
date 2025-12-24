763cc415ecfd693eef5989fe64fee8e1230757df
William Woodruff <william@yossarian.net>
2025-12-23 22:10:55 -0500

    more dechaff
    Signed-off-by: William Woodruff <william@yossarian.net>

--- crates/zizmor/tests/integration/audit/stale_action_refs.rs
+++ crates/zizmor/tests/integration/audit/stale_action_refs.rs
@@ -10,39 +10,15 @@ fn test_pedantic_persona() -> anyhow::Result<()> {
             .args(["--persona=pedantic"])
             .run()?,
         @r"
-    error[unpinned-uses]: unpinned action reference
-      --> @@INPUT@@:15:13
-       |
-    15 |     - uses: actions/checkout@main
-       |             ^^^^^^^^^^^^^^^^^^^^^ action is not pinned to a hash (required by blanket policy)
-       |
-       = note: audit confidence → High
-
-    error[unpinned-uses]: unpinned action reference
-      --> @@INPUT@@:18:13
-       |
-    18 |     - uses: actions/checkout@v4
-       |             ^^^^^^^^^^^^^^^^^^^ action is not pinned to a hash (required by blanket policy)
-       |
-       = note: audit confidence → High
-
-    error[unpinned-uses]: unpinned action reference
-      --> @@INPUT@@:21:13
-       |
-    21 |     - uses: actions/checkout@v4.2.2
-       |             ^^^^^^^^^^^^^^^^^^^^^^^ action is not pinned to a hash (required by blanket policy)
-       |
-       = note: audit confidence → High
-
     help[stale-action-refs]: commit hash does not point to a Git tag
-      --> @@INPUT@@:34:13
+      --> @@INPUT@@:34:15
        |
-    34 |     - uses: actions/checkout@009b9ae9e446ad8d9b8c809870b0fbcc5e03573e
-       |             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ this step
+    34 |       - uses: actions/checkout@009b9ae9e446ad8d9b8c809870b0fbcc5e03573e
+       |               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ this step
        |
        = note: audit confidence → High
 
-    4 findings: 0 informational, 1 low, 0 medium, 3 high
+    4 findings (3 ignored): 0 informational, 1 low, 0 medium, 0 high
     "
     );
 
