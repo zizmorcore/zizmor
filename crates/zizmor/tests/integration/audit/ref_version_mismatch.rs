8fcc0a3bbdd5a56accd1c8dd4c7bb7d5ff7eddd7
William Woodruff <william@yossarian.net>
2026-04-02 15:58:09 -0400

    Handle unknown refs in ref-version-mismatch
    Fixes #1853.
    
    Signed-off-by: William Woodruff <william@yossarian.net>

--- crates/zizmor/tests/integration/audit/ref_version_mismatch.rs
+++ crates/zizmor/tests/integration/audit/ref_version_mismatch.rs
@@ -33,7 +33,18 @@ fn test_ref_version_mismatch() -> Result<()> {
        = note: audit confidence → High
        = note: this finding has an auto-fix
 
-    3 findings (1 suppressed, 2 fixable): 0 informational, 0 low, 1 medium, 1 high
+    warning[ref-version-mismatch]: action's hash pin does not match version comment
+      --> @@INPUT@@:25:77
+       |
+    25 |       - uses: actions/setup-node@1a4442cacd436585916779262731d5b162bc6ec7 # v300000.8.1
+       |         -----------------------------------------------------------------   ^^^^^^^^^^^ points to unknown ref
+       |         |
+       |         is pointed to by tag v3.8.2
+       |
+       = note: audit confidence → High
+       = note: this finding has an auto-fix
+
+    4 findings (1 suppressed, 3 fixable): 0 informational, 0 low, 2 medium, 1 high
     "
     );
 
@@ -52,6 +63,33 @@ fn test_nested_annotated_tags() -> Result<()> {
             ))
             .run()?,
         @"No findings to report. Good job! (1 suppressed)"
+    );
+
+    Ok(())
+}
+
+#[cfg_attr(not(feature = "gh-token-tests"), ignore)]
+#[test]
+fn test_issue_1853() -> Result<()> {
+    insta::assert_snapshot!(
+        zizmor()
+            .offline(false)
+            .input(input_under_test("ref-version-mismatch/issue-1853-repro.yml"))
+            .run()?,
+        @"
+    warning[ref-version-mismatch]: action's hash pin does not match version comment
+      --> @@INPUT@@:14:75
+       |
+    14 |         uses: actions/setup-go@4a3601121dd01d1626a1e23e37211e3254c1c06c # v9.9.9
+       |         ---------------------------------------------------------------   ^^^^^^ points to unknown ref
+       |         |
+       |         is pointed to by tag v6.4.0
+       |
+       = note: audit confidence → High
+       = note: this finding has an auto-fix
+
+    2 findings (1 suppressed, 1 fixable): 0 informational, 0 low, 1 medium, 0 high
+    "
     );
 
     Ok(())
