42ea90f98c9d97710146d4c45c1998b5abb00603
William Woodruff <william@yossarian.net>
2026-04-14 18:16:17 -0400

    Add a workflow fast-path benchmark
    Signed-off-by: William Woodruff <william@yossarian.net>

--- bench/test_bench_online.py
+++ bench/test_bench_online.py
@@ -45,3 +45,26 @@ def test_zizmor_online_cpython_48f88310044c(benchmark):
         warmup_rounds=2,
         iterations=10,
     )
+
+
+@pytest.mark.skipif("GH_TOKEN" not in os.environ, reason="GH_TOKEN not set")
+def test_zizmor_online_collect_workflows_fast_path(benchmark):
+    """
+    Tests the performance of the `--collect=workflows` fast path, which should
+    be much faster than retrieving the entire repository.
+    """
+
+    benchmark.pedantic(
+        zizmor,
+        args=(
+            [
+                "--collect=workflows",
+                "--format=plain",
+                "--no-exit-codes",
+                "--no-config",
+                "python/cpython@48f88310044c",
+            ],
+        ),
+        warmup_rounds=2,
+        iterations=10,
+    )
