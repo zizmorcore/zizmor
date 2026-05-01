f8f91eea371f15057f4b08a2a47c938b043b9713
William Woodruff <william@yossarian.net>
2026-04-30 23:30:44 -0400

    Another heuristic test
    Signed-off-by: William Woodruff <william@yossarian.net>

--- crates/zizmor/tests/integration/audit/cache_poisoning.rs
+++ crates/zizmor/tests/integration/audit/cache_poisoning.rs
@@ -630,10 +630,10 @@ fn test_issue_1940() -> anyhow::Result<()> {
 }
 
 #[test]
-fn test_tag_event_heuristics() -> anyhow::Result<()> {
+fn test_trigger_heuristics_tag_only() -> anyhow::Result<()> {
     insta::assert_snapshot!(
         zizmor()
-            .input(input_under_test("cache-poisoning/tag-event-heuristics.yml"))
+            .input(input_under_test("cache-poisoning/trigger-heuristics/tag-only.yml"))
             .run()?,
         @r#"
     error[cache-poisoning]: runtime artifacts potentially vulnerable to a cache poisoning attack
@@ -655,6 +655,46 @@ fn test_tag_event_heuristics() -> anyhow::Result<()> {
     20 | |           # tag events correspond to releases.
     21 | |           sccache: ${{ startsWith(github.ref, 'refs/tags/') }}
        | |_______________________________________________________________- enables caching explicitly here
+       |
+       = note: audit confidence → Low
+       = note: this finding has an auto-fix
+
+    3 findings (2 suppressed, 1 fixable): 0 informational, 0 low, 0 medium, 1 high
+    "#
+    );
+
+    Ok(())
+}
+
+#[test]
+fn test_trigger_heuristics_tag_and_release() -> anyhow::Result<()> {
+    insta::assert_snapshot!(
+        zizmor()
+            .input(input_under_test(
+                "cache-poisoning/trigger-heuristics/tag-and-release.yml"
+            ))
+            .run()?,
+        @r#"
+    error[cache-poisoning]: runtime artifacts potentially vulnerable to a cache poisoning attack
+      --> @@INPUT@@:17:9
+       |
+     3 | / on:
+     4 | |   push:
+     5 | |     branches:
+     6 | |       - master
+     7 | |     tags:
+     8 | |       - "*"
+     9 | |   release:
+       | |__________- generally used when publishing artifacts generated at runtime
+    ...
+    17 |         - uses: PyO3/maturin-action@e83996d129638aa358a18fbd1dfb82f0b0fb5d3b # v1.51.0
+       |           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ this step
+    18 | /         with:
+    19 | |           # we would consider this safe *if* there was only a tag trigger for this workflow,
+    20 | |           # but since there's also a release trigger we can't assert that all release
+    21 | |           # events will also be tag events.
+    22 | |           sccache: ${{ !startsWith(github.ref, 'refs/tags/') }}
+       | |________________________________________________________________- may enable caching here
        |
        = note: audit confidence → Low
        = note: this finding has an auto-fix
