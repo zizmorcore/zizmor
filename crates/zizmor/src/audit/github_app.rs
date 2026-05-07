021d665613af4f0336b5496c7c2d54ae663fdd0b
William Woodruff <william@yossarian.net>
2026-05-07 07:37:19 -0700

    Add TODO

--- crates/zizmor/src/audit/github_app.rs
+++ crates/zizmor/src/audit/github_app.rs
@@ -41,6 +41,7 @@ impl GitHubApp {
             // TODO: Flag getsentry/action-github-app-token as well? It doesn't support any of
             //       the scoping that create-github-app-token does, so we could perhaps
             //       nudge users away from it.
+            // TODO: Flag wow-actions/use-app-token for similar reasons.
             vec![]
         };
 
