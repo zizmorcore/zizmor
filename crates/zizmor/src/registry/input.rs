4223dddc777989613ec84b38a4a1d80d9b24f9b9
William Woodruff <william@yossarian.net>
2026-05-07 20:24:34 -0700

    Include input key in validation warning

--- crates/zizmor/src/registry/input.rs
+++ crates/zizmor/src/registry/input.rs
@@ -392,7 +392,7 @@ impl InputGroup {
                 Ok(())
             }
             Err(e @ CollectionError::Schema { .. }) if !strict => {
-                tracing::warn!("failed to validate input as {kind}: {e}");
+                tracing::warn!("failed to validate {key} as {kind}: {e}");
                 Ok(())
             }
             Err(e) => Err(CollectionError::Inner(e.into(), key.to_string(), kind)),
