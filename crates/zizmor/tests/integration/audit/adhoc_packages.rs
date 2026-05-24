463f8cdf44813506f33f3729a3d621b7048173b1
Connor Shea <2977353+connorshea@users.noreply.github.com>
2026-05-23 22:55:43 -0600

    Add a few more test cases to the insta snapshots.

--- crates/zizmor/tests/integration/audit/adhoc_packages.rs
+++ crates/zizmor/tests/integration/audit/adhoc_packages.rs
@@ -6,7 +6,7 @@ fn test_adhoc_packages() -> anyhow::Result<()> {
         zizmor()
             .input(input_under_test("adhoc-packages.yml"))
             .run()?,
-        @r"
+        @r#"
     help[adhoc-packages]: ad-hoc package installation outside of a lockfile
       --> @@INPUT@@:18:14
        |
@@ -30,8 +30,8 @@ fn test_adhoc_packages() -> anyhow::Result<()> {
     help[adhoc-packages]: ad-hoc package installation outside of a lockfile
       --> @@INPUT@@:26:14
        |
-    26 |         run: gem install rake -v 13.0.6
-       |         ---  ^^^^^^^^^^^^^^^^^^^^^^^^^^ installs a package outside of a lockfile
+    26 |         run: gem install rake:13.0.6
+       |         ---  ^^^^^^^^^^^^^^^^^^^^^^^ installs a package outside of a lockfile
        |         |
        |         this step
        |
@@ -40,15 +40,47 @@ fn test_adhoc_packages() -> anyhow::Result<()> {
     help[adhoc-packages]: ad-hoc package installation outside of a lockfile
       --> @@INPUT@@:30:14
        |
-    30 |         run: gem install --no-document rake
+    30 |         run: gem install rake -v 13.0.6
+       |         ---  ^^^^^^^^^^^^^^^^^^^^^^^^^^ installs a package outside of a lockfile
+       |         |
+       |         this step
+       |
+       = note: audit confidence → High
+
+    help[adhoc-packages]: ad-hoc package installation outside of a lockfile
+      --> @@INPUT@@:34:14
+       |
+    34 |         run: gem install --no-document rake
        |         ---  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ installs a package outside of a lockfile
        |         |
        |         this step
        |
        = note: audit confidence → High
 
-    4 findings: 0 informational, 4 low, 0 medium, 0 high
-    "
+    help[adhoc-packages]: ad-hoc package installation outside of a lockfile
+      --> @@INPUT@@:40:11
+       |
+    38 |         run: |
+       |         --- this step
+    39 |           echo "Hello"
+    40 |           gem install foo
+       |           ^^^^^^^^^^^^^^^ installs a package outside of a lockfile
+       |
+       = note: audit confidence → High
+
+    help[adhoc-packages]: ad-hoc package installation outside of a lockfile
+      --> @@INPUT@@:41:11
+       |
+    38 |         run: |
+       |         --- this step
+    ...
+    41 |           gem install bar
+       |           ^^^^^^^^^^^^^^^ installs a package outside of a lockfile
+       |
+       = note: audit confidence → High
+
+    7 findings: 0 informational, 7 low, 0 medium, 0 high
+    "#
     );
 
     Ok(())
