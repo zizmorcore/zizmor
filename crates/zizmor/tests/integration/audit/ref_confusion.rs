1d306e267ba6517fa56324d94a3006710231abc4
Reuben Wong <reuben.wong.ys@gmail.com>
2026-08-10 07:53:34 +0800

    feat(ref-confusion): add audit for pre-commit-config (#2274)

--- crates/zizmor/tests/integration/audit/ref_confusion.rs
+++ crates/zizmor/tests/integration/audit/ref_confusion.rs
@@ -27,6 +27,32 @@ fn test_ref_confusion() -> Result<()> {
 
 #[cfg_attr(not(feature = "gh-token-tests"), ignore)]
 #[test]
+fn test_ref_confusion_pre_commit() -> Result<()> {
+    insta::assert_snapshot!(
+        zizmor()
+            .input(input_under_test("ref-confusion/pre-commit/.pre-commit-config.yaml"))
+            .offline(NetworkMode::AssertOnline)
+            .run()?,
+        @"
+    warning[ref-confusion]: git ref for action with ambiguous ref type
+     --> @@INPUT@@:3:10
+      |
+    2 |   - repo: https://github.com/woodruffw/gha-hazmat
+      |           --------------------------------------- this repo
+    3 |     rev: confusable
+      |          ^^^^^^^^^^ uses a ref that's provided by both the branch and tag namespaces
+      |
+      = note: audit confidence → High
+
+    1 finding: 0 informational, 0 low, 1 medium, 0 high
+    "
+    );
+
+    Ok(())
+}
+
+#[cfg_attr(not(feature = "gh-token-tests"), ignore)]
+#[test]
 fn test_issue_518_repro() -> Result<()> {
     insta::assert_snapshot!(
         zizmor()
