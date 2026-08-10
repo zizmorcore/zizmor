fb1f3d83383c5686a434a817d4dd663f462e83c1
William Woodruff <william@yossarian.net>
2026-08-09 22:25:49 -0400

    known-vulnerable-actions: lift fix tests to integration (#2287)

--- crates/zizmor/tests/integration/audit/known_vulnerable_actions.rs
+++ crates/zizmor/tests/integration/audit/known_vulnerable_actions.rs
@@ -52,3 +52,130 @@ fn test_config_allow() -> Result<()> {
 
     Ok(())
 }
+
+#[cfg(feature = "gh-token-tests")]
+#[test]
+fn test_fix_version_pin() -> anyhow::Result<()> {
+    use crate::common::WorkspaceBuilder;
+
+    // Note: this ignores `unpinned-uses` because of bug #2286.
+    // See: <https://github.com/zizmorcore/zizmor/issues/2286>
+    let workflow_content = r#"
+name: Test
+on: push
+permissions: {}
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Commit pinned action
+        uses: actions/download-artifact@v4.0.0 # zizmor: ignore[unpinned-uses]
+"#;
+
+    let workspace = WorkspaceBuilder::new().is_git_repo(true).build()?;
+    workspace.add_file(".github/workflows/test.yml", workflow_content);
+
+    insta::assert_snapshot!(
+        &workspace.diff(".github/workflows/test.yml", |workspace| {
+            zizmor()
+                .args(["--fix=all"])
+                .offline(NetworkMode::AssertOnline)
+                .input(workspace.path())
+                .run()
+        })?,
+        @"
+    @@ -7,4 +7,4 @@
+         runs-on: ubuntu-latest
+         steps:
+           - name: Commit pinned action
+    -        uses: actions/download-artifact@v4.0.0 # zizmor: ignore[unpinned-uses]
+    +        uses: actions/download-artifact@v4.1.3 # zizmor: ignore[unpinned-uses]
+    "
+    );
+
+    Ok(())
+}
+
+#[cfg(feature = "gh-token-tests")]
+#[test]
+fn test_fix_commit_pin() -> anyhow::Result<()> {
+    use crate::common::WorkspaceBuilder;
+
+    let workflow_content = r#"
+name: Test
+on: push
+permissions: {}
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Commit pinned action
+        uses: actions/download-artifact@7a1cd3216ca9260cd8022db641d960b1db4d1be4  # v4.0.0
+"#;
+
+    let workspace = WorkspaceBuilder::new().is_git_repo(true).build()?;
+    workspace.add_file(".github/workflows/test.yml", workflow_content);
+
+    insta::assert_snapshot!(
+        &workspace.diff(".github/workflows/test.yml", |workspace| {
+            zizmor()
+                .args(["--fix=all"])
+                .offline(NetworkMode::AssertOnline)
+                .input(workspace.path())
+                .run()
+        })?,
+        @"
+    @@ -7,4 +7,4 @@
+         runs-on: ubuntu-latest
+         steps:
+           - name: Commit pinned action
+    -        uses: actions/download-artifact@7a1cd3216ca9260cd8022db641d960b1db4d1be4  # v4.0.0
+    +        uses: actions/download-artifact@87c55149d96e628cc2ef7e6fc2aab372015aec85  # v4.1.3
+    "
+    );
+
+    Ok(())
+}
+
+// TODO: test_fix_commit_pin_subpath
+
+#[cfg(feature = "gh-token-tests")]
+#[test]
+fn test_fix_commit_pin_no_comment() -> anyhow::Result<()> {
+    use crate::common::WorkspaceBuilder;
+
+    let workflow_content = r#"
+name: Test
+on: push
+permissions: {}
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Commit pinned action
+        uses: actions/download-artifact@7a1cd3216ca9260cd8022db641d960b1db4d1be4
+"#;
+
+    let workspace = WorkspaceBuilder::new().is_git_repo(true).build()?;
+    workspace.add_file(".github/workflows/test.yml", workflow_content);
+
+    insta::assert_snapshot!(
+        &workspace.diff(".github/workflows/test.yml", |workspace| {
+            zizmor()
+                .args(["--fix=all"])
+                .offline(NetworkMode::AssertOnline)
+                .input(workspace.path())
+                .run()
+        })?,
+        @"
+    @@ -7,4 +7,4 @@
+         runs-on: ubuntu-latest
+         steps:
+           - name: Commit pinned action
+    -        uses: actions/download-artifact@7a1cd3216ca9260cd8022db641d960b1db4d1be4
+    +        uses: actions/download-artifact@87c55149d96e628cc2ef7e6fc2aab372015aec85
+    "
+    );
+
+    Ok(())
+}
